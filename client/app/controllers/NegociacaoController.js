5ad9cbda354548c6d0f171046500c2e8830df79f
Savio <kiqsavio@gmail.com>
2023-12-29 16:33:12 -0300

    criando metodos e ajustando o sistema de datas

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,7 +1,27 @@
 class NegociacaoController {
 
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
     adiciona(event) {
         event.preventDefault();
-        alert('Chamei ação no controller');
+
+        let data = new Date(...this._inputData.value.split('-').map(function(item, indice) {
+                return item - item % 2;
+            })
+        );
+        console.log(data);
+
+        let negociacao = new Negociacao(
+            this._inputData.value,
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+
+        console.log(negociacao);
     }
 }
