90c44d57712c4b585e927d1e1bb5f83160cb9df1
Savio <kiqsavio@gmail.com>
2024-01-04 09:27:22 -0300

    criando DateConverter e tratamento de datas

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -10,18 +10,14 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        let data = new Date(...this._inputData.value.split('-').map(function(item, indice) {
-                return item - item % 2;
-            })
-        );
-        console.log(data);
-
         let negociacao = new Negociacao(
-            this._inputData.value,
+            DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
 
-        console.log(negociacao);
+        let diaMesAno = DateConverter.paraTexto(negociacao.data);
+
+        console.log(diaMesAno);
     }
 }
